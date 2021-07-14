2ddd5df7eace74ccf49403b6d5c1c12c0fc9b8f3
Mahan M.V <73701328+Mahan2006@users.noreply.github.com>
2021-07-14 14:11:40 +0530

    Update __main__.py

--- LaylaRobot/__main__.py
+++ LaylaRobot/__main__.py
@@ -81,7 +81,7 @@ I'm a group manager bot maintained by [BeatKing Mahan](tg://user?id=1773361002).
 
 For more commands click /help**...
 
-**Keep in mind this bot is made only for @MahanMVGroup.**
+**Keep in mind this bot is made only for @KannadaMVRequest.**
 """
 
 
@@ -95,7 +95,7 @@ buttons = [
     [
         InlineKeyboardButton(text="About", callback_data="layla_"),
         InlineKeyboardButton(
-            text="Support", url=f"https://t.me/MahanMVGroup"),
+            text="Support", url=f"https://t.me/KannadaMVRequest"),
 
     ],
     [
